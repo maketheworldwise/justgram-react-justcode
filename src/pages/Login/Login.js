14c1e02821432a6db053a189e3472d703e6269d7
Kevin Ahn <maketheworldwise@gmail.com>
2022-08-18 12:41:46 +0900

    :sparkles: 아이디와 비밀번호 검증을 통한 버튼 활성화 여부 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "./Login.scss";
 
 import LogoText from "../../components/logo/LogoText";
 
 function Login() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  const handleIdInput = (e) => {
+    const idValue = e.target.value;
+    setId(idValue);
+    idValue.includes("@") && pw.length >= 5 ? setIsValid(true) : setIsValid(false);
+  };
+
+  const handlePwInput = (e) => {
+    const pwValue = e.target.value;
+    setPw(pwValue);
+    id.includes("@") && pw.length >= 5 ? setIsValid(true) : setIsValid(false);
+  };
+
   return (
     <div className="base">
       <div className="login">
@@ -11,9 +28,9 @@ function Login() {
         </div>
         <div className="form">
           <div className="content">
-            <input className="user-info" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-            <input className="user-info" type="password" placeholder="비밀번호" />
-            <button className="send-info" type="submit" disabled>
+            <input className="user-info" type="text" placeholder="전화번호, 사용자 이름 또는 이메일" onChange={handleIdInput} />
+            <input className="user-info" type="password" placeholder="비밀번호" onChange={handlePwInput} />
+            <button className="send-info" type="submit" disabled style={{ opacity: isValid ? "100%" : "50%" }}>
               로그인
             </button>
             <a className="find-password" href="#?">
